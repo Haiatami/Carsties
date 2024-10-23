7b45ecc2a2af74cb9dc2f833214a33e12d116354
Haiatami <tahoanghai755@gmail.com>
2024-10-23 22:11:45 +0700

    Client side identity with NextAuth

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -7,11 +7,19 @@ import React from "react";
 import { AiOutlineCar } from "react-icons/ai";
 
 export default function Logo() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  function doReset() {
+    if (pathname !== "/") router.push("/");
+    reset();
+  }
+
   const reset = useParamsStore((state) => state.reset);
 
   return (
     <div
-      onClick={reset}
+      onClick={doReset}
       className="flex items-center gap-2 text-3xl font-semibold text-red-500 cursor-pointer"
     >
       <AiOutlineCar size={34} />
