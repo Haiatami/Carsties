ac888ff0313a27b528c7218fadd72baa4d71f1d7
Haiatami <tahoanghai755@gmail.com>
2024-10-28 22:52:19 +0700

    Adding the bidsnotifications to the client

--- frontend/web-app/types/index.ts
+++ frontend/web-app/types/index.ts
@@ -9,16 +9,33 @@ export type Auction = {
   seller: string;
   winner?: string;
   soldAmount: number;
-  currentHighBid: number;
+  currentHighBid: any;
   createdAt: string;
   updatedAt: string;
   auctionEnd: string;
   status: string;
   make: string;
-  model: string; 
+  model: string;
   year: number;
   color: string;
   mileage: number;
   imageUrl: string;
   id: string;
+};
+
+export type Bid = {
+  id: string;
+  auctionId: string;
+  bidder: string;
+  bidTime: string;
+  amount: number;
+  bidStatus: string;
+};
+
+export type AuctionFinished = {
+  itemSold: boolean;
+  auctionId: string;
+  winner?: string;
+  seller: string;
+  amount?: number;
 };
